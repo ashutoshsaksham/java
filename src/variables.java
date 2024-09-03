a77ec714e1a860c6e500ebe9f6e0a6949f706696
ashutoshsaksham22 <ashutoshsaksham22@gmail.com>
2024-09-04 02:21:37 +0530

    java course. first program getting started with variables

---
+++ src/variables.java
@@ -0,0 +1,29 @@
+public class variables {
+
+    public static void main(String[] args){
+
+        boolean isPassed = true;
+        int count = 333;
+        byte marks = 23;
+
+        float pi = 3.14f; // we have to use f as literal in float it rounder off to 6 7 decimals
+
+        double secondPi = 3.14;
+
+        char firstChar = 'A';
+
+        // Implicit data type conversion
+
+        int countTwo = 134;
+        long countLong = countTwo;
+
+        // Explicit datatype conversion
+
+        long countThree = 23232;
+        int countConversion = (int)countThree; // in which type we want to convert, it is called type-casting
+
+
+
+    }
+
+}
