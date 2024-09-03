a443b58724eccd6cbe5a7d0c43ce11dfacaacdad
ashutoshsaksham22 <ashutoshsaksham22@gmail.com>
2024-09-04 02:30:08 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/hello.java

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,15 @@ public class hello {
 
     public static void main(String[] args){
         System.out.println("My first program");
+
+
+
+        // java is case sensitive hence age and AGE are two different variable
+// variable can;t use whitespace eg: int age old is wrong
+// it cannt be a keyword
+// variable no should not start with number eg: int 2nd is wrong way
+// use camelCase while creating multiple letter word
+//second_allowed$allowed only specific character allowed
     }
 
 }
